f5f958bf8e7e6707c355ceeffc1ccd04c0f4374c
Andreas Verhoeven <ave@aveapps.com>
2024-01-13 14:13:06 +0100

    Added AveIlity

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "AveSwiftHelpers",
+    name: "AveUtility",
 	platforms: [
 		.iOS(.v11),
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AveSwiftHelpers",
-            targets: ["AveSwiftHelpers"]),
+            name: "AveUtility",
+            targets: ["AveUtility"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AveSwiftHelpers",
+            name: "AveUtility",
             dependencies: [],
 			path: "Sources"),
     ]
